251b56a7ffcf8155fa907512f9310948f733ddbf
Patipat-224 <pwilaysoongnune@gmail.com>
2026-09-23 20:24:40 +0700

    c4e2

---
+++ discovery_piscine/cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+a = int(input("Give me the first number: "))
+b = int(input("Give me the second number: "))
+print("Thank you!")
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} / {b} = {a // b}")
+print(f"{a} * {b} = {a * b}")
