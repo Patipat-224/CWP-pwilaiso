1d794dda727453d5621791bf170a020f42c2a915
Patipat-224 <pwilaysoongnune@gmail.com>
2026-09-23 17:11:08 +0700

    cell02ex03output

--- discovery_piscine/cell02/ex03/mult.py
+++ discovery_piscine/cell02/ex03/mult.py
@@ -5,6 +5,8 @@ num2 = int(input("Enter the second number:\n"))
 
 result = num1 * num2
 
+print(str(num1) + " x " + str(num2) + " = " + str(result))
+
 if result > 0:
     print("The result is positive.")
 elif result < 0:
